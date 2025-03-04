7769e79033ae0d24b5aa66478b5a4d99dc947ddd
Torlogo <ylan.dessenne.spam@gmail.com>
2025-03-04 16:32:04 +0100

    feat(For.java) : add For class with a simple loop

---
+++ src/fr/ynov/java/easy/For.java
@@ -0,0 +1,9 @@
+package fr.ynov.java.easy;
+
+public class For {
+    public static void main(String[] args) {
+        for (int x = 1; x < 11; x++) {
+            System.out.println(x);
+        }
+    }
+}
