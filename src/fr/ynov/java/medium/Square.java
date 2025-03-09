ef5ddc020363ce167a45b84a4ec88cc4975c3011
Torlogo <ylan.dessenne.spam@gmail.com>
2025-03-09 19:26:12 +0100

    feat(Square.java) : add Square class to calculate the square of a user-input number, this is the first version with static method

---
+++ src/fr/ynov/java/medium/Square.java
@@ -0,0 +1,18 @@
+package fr.ynov.java.medium;
+
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args) {
+        System.out.println(square());
+    }
+    public static int square() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int x = sc.nextInt();
+        System.out.println("Your number squared is: ");
+        return x * x;
+    }
+
+}
+
