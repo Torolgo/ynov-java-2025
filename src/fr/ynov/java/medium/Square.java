a2d8a5125c2f5ce3961e95d0c54bbad879cf9df7
Torlogo <ylan.dessenne.spam@gmail.com>
2025-03-09 19:29:07 +0100

    feat(Square.java) : refactor square method to instance method and update main method for object-oriented approach (second version with non-static method)

--- src/fr/ynov/java/medium/Square.java
+++ src/fr/ynov/java/medium/Square.java
@@ -3,10 +3,21 @@ package fr.ynov.java.medium;
 import java.util.Scanner;
 
 public class Square {
+//    public static void main(String[] args) {
+//        System.out.println(square());
+//    }
+//    public static int square() {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number: ");
+//        int x = sc.nextInt();
+//        System.out.println("Your number squared is: ");
+//        return x * x;
+//    }
     public static void main(String[] args) {
-        System.out.println(square());
+        Square square = new Square();
+        System.out.println(square.square());
     }
-    public static int square() {
+    public int square() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int x = sc.nextInt();
