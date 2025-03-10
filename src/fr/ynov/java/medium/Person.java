1febd341d46738b0620a272619e42ed8baeae3e5
Torlogo <ylan.dessenne.spam@gmail.com>
2025-03-10 11:27:39 +0100

    feat(Person.java) : update Person class to use birthDate instead of age and add method to calculate age

--- src/fr/ynov/java/medium/Person.java
+++ src/fr/ynov/java/medium/Person.java
@@ -1,12 +1,15 @@
 package fr.ynov.java.medium;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Person {
     private final String name;
+    private final LocalDate birthDate;
     private final String gender;
     private final float height;
     private final float weight;
     private static nationality nationality;
-    private final int age;
 
     public enum nationality {
         FRENCH("French"),
@@ -23,18 +26,21 @@ public class Person {
         }
     };
 
-    public Person(String name, int age, String gender, float height, float weight, nationality nationality) {
+    public Person(String name, LocalDate birthDate, String gender, float height, float weight, nationality nationality) {
         this.name = name;
-        this.age = age;
+        this.birthDate = birthDate;
         this.gender = gender;
         this.height = height;
         this.weight = weight;
         Person.nationality = nationality;
     };
 
+    public int age(LocalDate birthDate) {
+        return Period.between(birthDate, LocalDate.now()).getYears();
+    };
     public void display() {
         System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
+        System.out.println("Age: " + age(birthDate) );
         System.out.println("Gender: " + gender);
         System.out.println("Height: " + height);
         System.out.println("Weight: " + weight);
@@ -42,7 +48,7 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        Person person = new Person("John", 25, "F", 1.75f, 75.0f, nationality.FRENCH);
+        Person person = new Person("John", LocalDate.of(1956, 1, 5), "F", 1.75f, 75.0f, nationality.FRENCH);
         person.display();
 
     }
